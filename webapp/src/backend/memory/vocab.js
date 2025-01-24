115d71277ee8a89c5945b4d87201fcca5611158c
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-24 18:30:05 -0500

    Initialize localStorage memory for vocabulary functions

---
+++ webapp/src/backend/memory/vocab.js
@@ -0,0 +1,8 @@
+const newActionWordsMemory = localStorage.getItem("newActionWordsMemory-Samaritan");
+const newArticleWordsMemory = localStorage.getItem("newArticleWordsMemory-Samaritan");
+const newAdjectiveWordsMemory = localStorage.getItem("newAdjectiveWordsMemory-Samaritan");
+const newSubjectWordsMemory = localStorage.getItem("newSubjectWordsMemory-Samaritan");
+
+function checkMemoryUnits () {
+  
+}
