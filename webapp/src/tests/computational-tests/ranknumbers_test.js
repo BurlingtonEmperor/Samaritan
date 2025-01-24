4e18fb08a4227bbf7f4c581ac45a4fc033d0db74
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-24 09:20:16 -0500

    Successfully tested rankNumbers function

---
+++ webapp/src/tests/computational-tests/ranknumbers_test.js
@@ -0,0 +1,23 @@
+function rankNumbers (numArray) {
+  let easyNumArray = numArray;
+  let rankingArray = [];
+  
+  if (easyNumArray.length < 2) {
+    return "too little data";
+  }
+  
+  for (let i = 0; i < easyNumArray.length; i++) {
+    let x = findLargestNumber(easyNumArray);
+  
+    let swapStoreNum = easyNumArray[easyNumArray.length - 1];
+    easyNumArray[easyNumArray.length - 1] = x[0];
+    easyNumArray[x[1]] = swapStoreNum;
+    rankingArray.push(x[0]);
+    easyNumArray.pop();
+  }
+  
+  return rankingArray;
+}
+
+rankNumbers([3, 5, 6, 7, 7, 8, 10]);
+alert(rankingArray);
