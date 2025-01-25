80c574aec13b9c0595eb1bea2479336254db60be
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-25 13:42:08 -0500

    Note to self: delete the old connections, they don't work anymore

--- public/static/src/backend/memory/threats.js
+++ public/static/src/backend/memory/threats.js
@@ -3,5 +3,5 @@ const memoryUnitNameArray = ["socialMediaSites", "assestSites", "resourceSites"]
 // Default (using no memory)
 let socialMediaSites = ["facebook", "instagram", "linkedin", "snapchat", "x.com"];
 let assetSites = ["github", "youtube"];
-let resourceSites = ["bluealliance", "frc", "first"];
+let resourceSites = ["bluealliance", "frc", "first", "chiefdelphi"];
 // End default
