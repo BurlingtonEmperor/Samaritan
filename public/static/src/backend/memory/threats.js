8c9d391fa41c10289a253a9caecce4a2457cda8e
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-25 13:49:32 -0500

    Add new data

--- public/static/src/backend/memory/threats.js
+++ public/static/src/backend/memory/threats.js
@@ -1,7 +1,7 @@
 const memoryUnitNameArray = ["socialMediaSites", "assestSites", "resourceSites"];
 
 // Default (using no memory)
-let socialMediaSites = ["facebook", "instagram", "linkedin", "snapchat", "x.com"];
+let socialMediaSites = ["facebook", "instagram", "linkedin", "snapchat", "x.com", "weebly"];
 let assetSites = ["github", "youtube"];
 let resourceSites = ["bluealliance", "frc", "first", "chiefdelphi"];
 // End default
