11a81d1947a37b9b56bc7335a4539fa0ff75cf08
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-25 23:01:36 -0500

    team skill compare function added

---
+++ public/static/src/backend/threat_assessment/team_skill_compare.js
@@ -0,0 +1,39 @@
+let skillMatchResult = "";
+
+class teamSkillCompare {
+  constructor (blueAlliance, redAlliance) {
+    this.blueAlliance = blueAlliance;
+    this.redAlliance = redAlliance;
+  }
+
+  runMatch () {
+    let blueAlliancePoints = 0;
+    let redAlliancePoints = 0;
+
+    for (let i = 0; i < this.blueAlliance.length; i++) {
+      let specificAllianceTeam = new teamSkillAssessment(this.blueAlliance[i]);
+      specificAllianceTeam.assessThreat();
+      blueAlliancePoints += ultimateResult;
+    }
+
+    for (let i = 0; i < this.redAlliance.length; i++) {
+      let specificAllianceTeam = new teamSkillAssessment(this.redAlliance[i]);
+      specificAllianceTeam.assessThreat();
+      redAlliancePoints += ultimateResult;
+    }
+
+    switch (true) {
+      case (blueAlliancePoints < redAlliancePoints):
+        skillMatchResult = "red likely";
+        break;
+      case (blueAlliancePoints > redAlliancePoints):
+        skillMatchResult = "blue likely";
+        break;
+      default:
+        skillMatchResult = "outcome unknown";
+        break;
+    }
+    
+    return "success";
+  }
+}
