c2175fea334af0041c62d668a3b2b72871009763
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-26 12:47:34 -0500

    test 4

--- public/static/src/backend/threat_assessment/team_skill_compare.js
+++ public/static/src/backend/threat_assessment/team_skill_compare.js
@@ -58,11 +58,11 @@ class teamSkillCompare {
 
       const ultimateResultForComparison = new Proxy({ result : ultimateResult.result }, { 
         set (target, prop, val) {
-          console.log(`ultimateResulForComparison.result changed from ${target[prop]} to ${val}`);
           blueAlliancePoints += val;    
           blueAllianceCounter++;
           finalBlueAlliancePoints = blueAlliancePoints;
           console.log(blueAlliancePoints);
+          console.log(`ultimateResulForComparison.result changed from ${target[prop]} to ${val}`);
           target[prop] = val;
           blueAllianceLoop();
         }
