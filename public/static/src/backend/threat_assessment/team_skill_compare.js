4e34313dd38132099b9276fa1610251d7fe4e7d1
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-25 23:10:16 -0500

    make test friendly

--- public/static/src/backend/threat_assessment/team_skill_compare.js
+++ public/static/src/backend/threat_assessment/team_skill_compare.js
@@ -13,12 +13,14 @@ class teamSkillCompare {
     for (let i = 0; i < this.blueAlliance.length; i++) {
       let specificAllianceTeam = new teamSkillAssessment(this.blueAlliance[i]);
       specificAllianceTeam.assessThreat();
+      console.log(ultimateResult);
       blueAlliancePoints += ultimateResult;
     }
 
     for (let i = 0; i < this.redAlliance.length; i++) {
       let specificAllianceTeam = new teamSkillAssessment(this.redAlliance[i]);
       specificAllianceTeam.assessThreat();
+      console.log(ultimateResult);
       redAlliancePoints += ultimateResult;
     }
 
