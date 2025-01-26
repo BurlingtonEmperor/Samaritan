05b305dd408e24a17e84564610766e49a13cdc14
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-26 09:58:49 -0500

    fix another error

--- public/static/src/backend/threat_assessment/team_skill_compare.js
+++ public/static/src/backend/threat_assessment/team_skill_compare.js
@@ -33,7 +33,7 @@ class teamSkillCompare {
         }
       });
 
-      await teamSkillAssessment(this.blueAlliance[i]);
+      await teamSkillAssessment(this.blueAlliance[blueAllianceCounter]);
     }
 
     let redAllianceCounter = 0;
@@ -47,7 +47,7 @@ class teamSkillCompare {
         }
       });
 
-      await teamSkillAssessment(this.redAlliance[i]);
+      await teamSkillAssessment(this.redAlliance[redAllianceCounter]);
     }
 
     switch (true) {
