5c017d2002ed55fdff1a10c7f55a36fb38509a8f
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-01-29 12:58:12 -0500

    findMostRelevantTeam function added

--- public/static/src/backend/threat_assessment/relevance/find_relevant_teams.js
+++ public/static/src/backend/threat_assessment/relevance/find_relevant_teams.js
@@ -1,10 +1,37 @@
-async function findRelevantTeams (candidateArray) {
-  let relevantTeamArray = [];
+let relevantTeamArray = [];
 
+async function prepareRelevantTeams (candidateArray) {
   for (let i = 0; i < candidateArray.length; i++) {
     await teamSkillAssessment(candidateArray[i], 1);
-    
+
+    let teamArrToPush = [];
+    teamArrToPush.push(candidateArray[i]);
+    teamArrToPush.push(ultimateResult.result);
+
+    relevantTeamArray.push(teamArrToPush);
   }
+}
+
+async function findMostRelevantTeam () {
+  switch (true) {
+    case (relevantTeamArray.length < 1):
+      return "abort";
+  }
+
+  let mostSkilledTeam = 0;
+  let skillTeamAwardNum = -1;
+
+  for (let i = 0; i < relevantTeamArray.length; i++) {
+    let teamAwardAmount = relevantTeamArray[i][1];
+    let actualTeamNumber = relevantTeamArray[i][0];
+    
+    if (teamAwardAmount > skillTeamAwardNum) {
+      mostSkilledTeam = actualTeamNumber;
+      skillTeamAwardNum = teamAwardAmount;
+    }
+  }
+
+  return mostSkilledTeam;
 }
 
 function relevancyCounter (relevantTeamNumber) {
