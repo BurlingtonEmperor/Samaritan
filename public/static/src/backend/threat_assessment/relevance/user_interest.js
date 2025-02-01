df71e3bce3adb2bbbf52a0d26dc7430de373714a
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-02-01 19:01:44 +0000

    Simplify user interest functions

--- public/static/src/backend/threat_assessment/relevance/user_interest.js
+++ public/static/src/backend/threat_assessment/relevance/user_interest.js
@@ -1,9 +1,12 @@
 let teamInterestArr = [2876];
 
-function addTeamCount (target_word) {
-  
+function addTeamCount (target_team) {
+  teamInterestArr.push(parseInt(target_team));
 }
 
-function suggestTeams (user_use_countArray) {
-  
+function suggestTeams () {
+  return teamInterestArr.sort((a,b) =>
+        teamInterestArr.filter(v => v===a).length
+      - teamInterestArr.filter(v => v===b).length
+  ).pop();
 }
