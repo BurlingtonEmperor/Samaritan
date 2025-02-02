22368d3f732bbd7d0fbbca00f5c5996e026858d2
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-02-02 17:50:47 -0500

    fix vercel static file issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const request = require('request');
